b588d45f66b571966dbe056d964bf0ecb587077c
carlosaux <52382443+carlosaux@users.noreply.github.com>
2020-04-19 00:53:21 -0500

    interfaz usuario plantilla

--- JS/abrir.js
+++ JS/abrir.js
@@ -1,5 +1,8 @@
-$menu-bar =
-$('.menu-bar').click(function(e) {
-    e.preventDefault();
-    $(".contenido").toggleClass("abrir");
-});
+$(document).ready(function () {
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+      $(".Alin-img").toggleClass("Alin-imgt");
+      $(".Alin-user").toggleClass("Alin-usert");
+    });
+  })
