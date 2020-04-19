80ad5fcf26cd782eb24bbb1c6d0f188ff50dfbc9
carlosaux <52382443+carlosaux@users.noreply.github.com>
2020-04-19 13:24:05 -0500

    plantilla interfaz usuario

---
+++ PHP/navsuperior.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg Alineacion navbar-color">
+        <button class="btn btn-primary boton_menu" id="menu-toggle"><img src="../Imagenes/Menu.png" alt=""
+            class="boton_img"></button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="logonav col-8">
+          <a class="navbar-brand" href="#"><img src="../Imagenes/Logo.png" alt="Logo" class="Logo"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse Alin-user col-2 Alin-usert" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto " style="padding-left:revert">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Carlos Aux
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#"><img src="../Imagenes/inicio.png" class="inicio" alt="">Mi
+                  informacion</a>
+                <a class="dropdown-item" href="#"><img src="../Imagenes/inicio.png" class="inicio" alt="">Another
+                  action</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="Alin-img col-2 Alin-imgt">
+          <a class="navbar-brand Alineacion" href="#"><img src="../Imagenes/Usuario.png" alt="Mensaje"
+              class="Usuario"></a>
+        </div>
+      </nav>
