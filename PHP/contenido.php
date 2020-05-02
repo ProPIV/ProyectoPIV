833374733f734566d31b37cd0107efa32a019ed6
carlosaux <52382443+carlosaux@users.noreply.github.com>
2020-05-01 22:48:29 -0500

    adtualizacion contenido

--- PHP/contenido.php
+++ PHP/contenido.php
@@ -1,8 +1,13 @@
+<?php 
+  $contenidoAdmin = file_get_contents('contenidoAdmin.php');
+  $rol = "admin";
+?>
 <div class="container-fluid">
-    <h1 class="mt-4">Proyect</h1>
-    <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on
-        larger screens. When toggled using the button below, the menu will change.</p>
-    <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional,
-        and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the
-        menu when clicked.</p>
+    <?php 
+        if ($rol="admin") {
+            echo $contenidoAdmin;
+        }elseif (condition) {
+            echo "Error";
+        }
+    ?>    
 </div>
