28ba6d7fc0ae84459ee861439364611eced6067f
carlosaux <52382443+carlosaux@users.noreply.github.com>
2020-05-01 22:51:41 -0500

    actualizacion de contenido admin

--- PHP/contenido.php
+++ PHP/contenido.php
@@ -1,10 +1,10 @@
 <?php 
   $contenidoAdmin = file_get_contents('contenidoAdmin.php');
-  $rol = "admin";
+  $rol = "admon";
 ?>
 <div class="container-fluid">
     <?php 
-        if ($rol="admin") {
+        if ($rol="admon") {
             echo $contenidoAdmin;
         }elseif (condition) {
             echo "Error";
