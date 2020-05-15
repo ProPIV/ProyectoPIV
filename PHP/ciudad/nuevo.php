9d9758fa99486092843fd27f1f1c7bf1b3ec32eb
Jorgeacero9 <jorgeacero9@gmail.com>
2020-05-14 19:57:35 -0500

    ciudad

--- PHP/ciudad/nuevo.php
+++ PHP/ciudad/nuevo.php
@@ -1,7 +1,7 @@
 <!-- quick email widget -->
 <div id="seccion-ciudad">
 	<div class="box-header">
-    	<i class="fa fa-building" aria-hidden="true">Gestión de ciudades </i>
+    	<i class="fa fa-building" aria-hidden="true">Gestión de las ciudades </i>
         <!-- tools box -->
         <div class="pull-right box-tools">
         	<button class="btn btn-info btn-sm btncerrar2" data-toggle="tooltip" title="Cerrar"><i class="fa fa-times"></i></button>
